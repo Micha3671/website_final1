0505ad2fe6469c80c67305c3b37ca0caf60b693d
Micha <michael.rigas@t-online.de>
2024-01-23 17:33:26 +0100

    change

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Referenzierung der Anker-Elemente
 var registerLink = document.getElementById('registerLink');
 var loginLink = document.getElementById('loginLink');
 
-// Beispiel: Hinzufügen eines Event Listeners auf das Registrieren-Link-Klick-Ereignis
 registerLink.addEventListener('click', function() {
-    // Hier kannst du den Code ausführen, der nach dem Klicken auf das Registrieren-Link ausgeführt werden soll
+
     console.log('Registrieren-Link wurde geklickt!');
 });
 
-// Beispiel: Hinzufügen eines Event Listeners auf das Anmelden-Link-Klick-Ereignis
+
 loginLink.addEventListener('click', function() {
-    // Hier kannst du den Code ausführen, der nach dem Klicken auf das Anmelden-Link ausgeführt werden soll
+
     console.log('Anmelden-Link wurde geklickt!');
 });
